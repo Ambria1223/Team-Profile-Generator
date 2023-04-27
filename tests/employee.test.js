3f4a502daaf7f2bf6c60506cde8ab1be8036535e
Ambria Johnson <1223ambria@gmail.com>
2023-04-27 09:06:13 -0400

    added tests

---
+++ tests/employee.test.js
@@ -0,0 +1,23 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
+
+getname(); {
+    console.log(`name: ${this.name}`);
+}
+
+getid(); {
+    console.log(`id: ${this.id}`);
+}
+
+getemail(); {
+    console.log(`email: ${this.email}`);
+}
+
+getRole(); {
+    console.log(`Employee: ${this.Employee}`)
+}
---
+++ tests/manager.test.js
@@ -0,0 +1,29 @@
+class Manager {
+    constructor(name, id, email, officeNumber) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.officeNumber = officeNumber;
+    }
+}
+
+getName(); {
+    console.log(`name: ${this.name}`);
+}
+
+getID(); {
+    console.log(`id: ${this.id}`);
+}
+
+getEmail(); {
+    console.log(`email: ${this.email}`);
+}
+
+getofficeNumber();
+{
+    console.log(`Office Number: ${this.officeNumber}`)
+}
+
+getRole(); {
+    console.log(`Manager: ${this.Manager}`)
+}
