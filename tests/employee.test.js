ef6484e1fc4d467f7e075dce6155979ecd680f06
Ambria Johnson <1223ambria@gmail.com>
2023-05-04 19:18:01 -0400

    making sure everything is pushed

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,3 +1,5 @@
+const { default: test } = require('tests/employee.test.js');
+
 class Employee {
     constructor(name, id, email) {
         this.name = name;
@@ -6,18 +8,27 @@ class Employee {
     }
 }
 
-getname(); {
+getName(); {
     console.log(`name: ${this.name}`);
 }
 
-getid(); {
+getId(); {
     console.log(`id: ${this.id}`);
 }
 
-getemail(); {
+getEmail(); {
     console.log(`email: ${this.email}`);
 }
 
 getRole(); {
     console.log(`Employee: ${this.Employee}`)
 }
+
+test('Employee class methods return expected values', () => {
+    const employee = new Employee('John Smith', '1234', 'john.smith@example.com');
+    expect(employee.getName()).toBe('John Smith');
+    expect(employee.getId()).toBe('1234');
+    expect(employee.getEmail()).toBe('john.smith@example.com');
+    expect(employee.getRole()).toBe('Employee');
+  });
+  
